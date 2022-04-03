e2ca1b6b2e7ce585362d02c46a3c360e786e2ee3
Nathan Yapi <nathanyapi.pro@gmail.com>
2022-04-03 11:48:45 -0400

    WIP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,26 +6,40 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import theme from "../src/theme";
 import createEmotionCache from "../src/createEmotionCache";
+import { GlobalStyles } from "@mui/material";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 interface MyAppProps extends AppProps {
-	emotionCache?: EmotionCache;
+  emotionCache?: EmotionCache;
 }
 
 export default function MyApp(props: MyAppProps) {
-	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-	return (
-		<CacheProvider value={emotionCache}>
-			<Head>
-				<meta name="viewport" content="initial-scale=1, width=device-width" />
-			</Head>
-			<ThemeProvider theme={theme}>
-				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-				<CssBaseline />
-				<Component {...pageProps} />
-			</ThemeProvider>
-		</CacheProvider>
-	);
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const inputGlobalStyles = (
+    <GlobalStyles
+      styles={{
+        "#__next": {
+          display: "flex",
+          flexDirection: "column",
+          flex: "1 1 0",
+          minHeight: "100vh",
+        },
+      }}
+    />
+  );
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        {inputGlobalStyles}
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,30 +6,37 @@ import Box from "@mui/material/Box";
 import Link from "../src/Link";
 import ProTip from "../src/ProTip";
 import Copyright from "../src/Copyright";
+import { Button } from "@mui/material";
 
 const Home: NextPage = () => {
-	return (
-		<Container maxWidth="lg">
-			<Box
-				sx={{
-					my: 4,
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "center",
-					alignItems: "center",
-				}}
-			>
-				<Typography variant="h4" component="h1" gutterBottom>
-					MUI v5 + Next.js with TypeScript example
-				</Typography>
-				<Link href="/about" color="secondary">
-					Go to the about page
-				</Link>
-				<ProTip />
-				<Copyright />
-			</Box>
-		</Container>
-	);
+  return (
+    <Container
+      maxWidth="lg"
+      sx={{
+        display: "flex",
+        flex: "1 1 0",
+        my: 4,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flex: "1 1 0",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom sx={{}}>
+          Slime
+        </Typography>
+        <Button variant="contained" component={Link} noLinkStyle href="/">
+          Go to the home page
+        </Button>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
 };
 
 export default Home;
